798a02df02ebd5f91a1a9827eb0e41aaf6164ba2
Nathan Brunet <nathan_brunet@outlook.fr>
2025-05-10 21:58:25 +0200

    parse_time

--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,11 @@
 from typing import Any, Text, Dict, List
 
 import json
+import re
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 class ActionShowAvailability(Action):
     def name(self)->Text:
@@ -19,3 +21,19 @@ class ActionShowAvailability(Action):
                 response += f"{day['day']} between {day['time'].split('-')[0]} and {day['time'].split('-')[1]}\n"
         dispatcher.utter_message(text="Here are the availabilities for next week :\n" + response)
         return []
+
+class ActionParseTime(Action):
+     def name(self)->Text:
+          return "action_parse_time"
+     def run(self,dispatcher : CollectingDispatcher,tracker: Tracker, domain: Dict[Text,any]) -> List[Dict[Text,any]]:
+            time = tracker.get_slot("time")
+            if not time:
+                 dispatcher.utter_message(text="Sorry, I couldn't find the time information.")
+                 return []
+            day_pattern = r"\b\w{4,}\b"
+            hour_pattern = r'\b([01]?\d|2[0-3])(:[0-5]\d)?\b'
+            day = re.findall(day_pattern,time)
+            hour_matches = re.findall(hour_pattern,time)
+            hour = ["".join(match) for match in hour_matches]
+            return [SlotSet("day",day),SlotSet("hour",hour)] 
+                    
