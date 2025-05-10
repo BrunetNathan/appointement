a2c0423842dc10a54d18c5b96c563e14d8c7230d
Nathan Brunet <nathan_brunet@outlook.fr>
2025-05-10 23:11:48 +0200

    appointement_canceled et final_confirmation

--- actions/actions.py
+++ actions/actions.py
@@ -35,5 +35,17 @@ class ActionParseTime(Action):
             day = re.findall(day_pattern,time)
             hour_matches = re.findall(hour_pattern,time)
             hour = ["".join(match) for match in hour_matches]
-            return [SlotSet("day",day),SlotSet("hour",hour)] 
-                    
+            return [SlotSet("day",day[0]),SlotSet("hour",hour[0])]
+     
+class ActionCheckAvailability(Action):
+    def name(self) -> Text:
+        return "action_check_availability"
+    def run(self,dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any],) -> List[Dict[Text, Any]]:
+        with open("data/doctors.json") as json_data:
+                data = json.load(json_data)
+                json_data.close()
+        doctor = tracker.get_slot("doctor")
+        day = tracker.get_slot("day")
+        hour = tracker.get_slot("hour")
+        print(f"{doctor} | {day} | {hour}")
+        return [SlotSet("appointement_availability",True)]
