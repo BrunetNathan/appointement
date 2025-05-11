9de6c611f1ba45c4a629fcd9d80acff961e37525
Nathan Brunet <nathan_brunet@outlook.fr>
2025-05-11 11:16:47 +0200

    ActionCheckAvailability

--- actions/actions.py
+++ actions/actions.py
@@ -2,10 +2,20 @@ from typing import Any, Text, Dict, List
 
 import json
 import re
+from datetime import datetime
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+
+def is_time_in_range(time_str, range_str):
+    time = datetime.strptime(time_str, "%H:%M").time()
+    start_str, end_str = range_str.split("-")
+    start_time = datetime.strptime(start_str.strip(), "%H:%M").time()
+    end_time = datetime.strptime(end_str.strip(), "%H:%M").time()
+
+    return start_time <= time <= end_time
+
 
 class ActionShowAvailability(Action):
     def name(self)->Text:
@@ -18,7 +28,7 @@ class ActionShowAvailability(Action):
         for doctor in data :
             response += f"{doctor['name']} is available :\n"
             for day in doctor['availability']:
-                response += f"{day['day']} between {day['time'].split('-')[0]} and {day['time'].split('-')[1]}\n"
+                response += f"{day['day'].title()} between {day['time'].split('-')[0]} and {day['time'].split('-')[1]}\n"
         dispatcher.utter_message(text="Here are the availabilities for next week :\n" + response)
         return []
 
@@ -47,5 +57,22 @@ class ActionCheckAvailability(Action):
         doctor = tracker.get_slot("doctor")
         day = tracker.get_slot("day")
         hour = tracker.get_slot("hour")
-        print(f"{doctor} | {day} | {hour}")
-        return [SlotSet("appointement_availability",True)]
+        for doctor_schedule in data :
+            if doctor in doctor_schedule['name'] :
+                for availability in doctor_schedule['availability']:
+                    if availability['day'] == day.lower():
+                        is_in_range = is_time_in_range(hour,availability['time'])
+                        return [SlotSet("appointement_availability",is_in_range)]
+        return [SlotSet("appointement_availability",False)]
+
+
+def checkappointement(doctor: str,day:str,hour:str):
+    with open("data/doctors.json", encoding="utf-8") as json_data:
+        data = json.load(json_data)
+        json_data.close()
+    for doctor_schedule in data :
+        if doctor in doctor_schedule['name'] :
+            for availability in doctor_schedule['availability']:
+                if availability['day'] == day.lower():
+                    return is_time_in_range(hour,availability['time'])
+    return False
